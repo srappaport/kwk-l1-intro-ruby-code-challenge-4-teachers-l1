895bdd541e19c34a5defda3ef27ea27f479a1dc6
srappaport <sarah.rappaport@greendot.org>
2018-07-11 19:31:11 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,6 +1,6 @@
 def least(coin_values, change, known_results)
   mincoins = change
-  if change.include?(coin_values) == true
+  if coin_values.include?(change) == true
     known_results[change]=1
   elsif known_results[change] >0
     puts known_results
