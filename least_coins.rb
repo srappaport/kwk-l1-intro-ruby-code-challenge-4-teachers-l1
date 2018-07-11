927e0c026af0041faba285c8c8f8f28e42ca7356
srappaport <sarah.rappaport@greendot.org>
2018-07-11 19:41:32 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -3,20 +3,18 @@ def least(coin_values, change, known_results)
   puts mincoins
   if coin_values.include?(change) == true
     known_results[change]=1
-    binding.pry
   elsif known_results[change] > 0
     puts 'yo'
     puts known_results
-    binding.pry
   else
     i = 0
     while i < change
       numcoins = 1 + least(coin_values,change-i, known_results)
-      binding.pry
       if numcoins < mincoins
         mincoins = numcoins
         known_results[change] = mincoins
         i +=1
+        break
       end
     end
   end
